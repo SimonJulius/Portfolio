cd12f9e83b3556fc161d6ddc5706846e949152cf
SimonJulius <simonjulius0511@gmail.com>
2020-11-05 01:15:46 +0100

    added a click event handler to menu

--- src/components/menu/menu.style.ts
+++ src/components/menu/menu.style.ts
@@ -1,6 +1,28 @@
 import styled from "styled-components";
 
+const MenuStyle = styled.div.attrs((props) => ({
+  className: props.className,
+}))`
+  display: flex;
+  flex-direction: column;
+  position: fixed;
+  top: 0px;
+  right: 0px;
+  background-color: #172a45;
+  z-index: 1;
+  width: 100%;
+  height: 90%;
+  font-size: 50px;
+  align-items: center;
+  justify-content: space-around;
+  a {
+    color: #64ffda;
+  }
+  .cancel {
+    align-self: flex-start;
+    font-size: 30px;
+    transform: translate(10px, -15px);
+  }
+`;
 
-const MenuStyle = styled.div``
-
-export default MenuStyle 
+export default MenuStyle;
--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import MenuStyle from "./menu.style";
 import MenuItem from "./menu-item";
+import { FaTimes } from "react-icons/fa";
+interface IMenuProps {
+  className: string;
+  hideMenuOnClick: ()=> void;
+}
 
-const Menu = () => {
+const Menu = ({ className, hideMenuOnClick }: IMenuProps) => {
   return (
-    <MenuStyle>
+    <MenuStyle className={className}>
+      <FaTimes className="cancel" onClick={hideMenuOnClick}/>
       <MenuItem fontSize={14} fontColor="#000" fontWeight="normal">
         Home
       </MenuItem>
