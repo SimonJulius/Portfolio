ac4966a33f6284b56606e260cdb5e154937ebf98
SimonJulius <simonjulius0511@gmail.com>
2020-11-05 01:17:35 +0100

    updated navbar, added state to handle hide and show menu bar

--- src/components/navbar/navbar.style.ts
+++ src/components/navbar/navbar.style.ts
@@ -1,5 +1,20 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
-const NavbarStyles = styled.nav``
+const NavbarStyles = styled.nav`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  color: white;
+  height: 50px;
+  margin-bottom: 10px;
+  margin-top: 0px;
+  .menu-mobile {
+    margin-right: 20px;
+  }
 
-export default NavbarStyles
+  .hidden {
+      display: none;
+  }
+`;
+
+export default NavbarStyles;
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,16 +1,40 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { IconContext } from "react-icons";
 import NavbarStyles from "./navbar.style";
 import Menu from "../menu/menu";
 import Logo from "../logo/logo";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Navbar = () => {
-    return (
-        <NavbarStyles>
-            <Logo />
-            <Menu />
-        </NavbarStyles>
-    )
+  const [menuClass, setMenuClass] = useState("hidden");
+  const [hamburgerClass, setHamburgerClass] = useState("shown");
+
+  const showMenuOnClick = () => {
+    setMenuClass((previousClass) => (previousClass = "shown"));
+    setHamburgerClass((previousClass) => (previousClass = "hidden"));
+  };
+
+  const hideMenuOnClick = () => {
+    setMenuClass((previousClass) => (previousClass = "hidden"));
+    setHamburgerClass((previousClass) => (previousClass = "shown"));
+  }
+
+  return (
+    <NavbarStyles>
+      <Logo />
+      <Menu className={menuClass} hideMenuOnClick={hideMenuOnClick}/>
+      <IconContext.Provider
+        value={{ color: "white", size: "25px", className: "react-icon" }}
+      >
+        <div className="menu-mobile">
+          <GiHamburgerMenu
+            onClick={showMenuOnClick}
+            className={hamburgerClass}
+          />
+        </div>
+      </IconContext.Provider>
+    </NavbarStyles>
+  );
 };
 
 export default Navbar;
